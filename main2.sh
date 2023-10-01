ffb5fdd0b67fa00dc65631a2534431091e3dbbcf
nelsenardian <ardiannelsen@gmail.com>
2023-10-01 14:23:14 +0700

    commit main2

--- main2.sh
+++ main2.sh
@@ -5,17 +5,22 @@ dir="$path/$name_of_directory"
 filename_excel="daily_market_price.xlsx"
 source_dir="/local/data/market"
 
+
 # Check if the directory exists
 if [ -d "$dir" ]; then
   echo "There is $name_of_directory Directory Exists!"
-  # Copy the file from source directory to target directory
-  cp "$source_dir/$filename_excel" "$path"
-  echo "File Moved Successfully" 
-else
+  else
   echo "$name_of_directory Directory Not Exists!"
   mkdir "$dir"
   echo "Directory $name_of_directory created."
   exit 1
 fi
 
+#Check if the source Excel file exists
+if [ -e "$source_dir/$filename_excel" ]; then
+  cp "$source_dir/$filename_excel" "$dir/"
+  echo "$filename_excel copied successfully"
 
+else
+  echo "Source file $source_dir not found."
+fi
